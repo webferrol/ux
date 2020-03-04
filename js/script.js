bb73b79577311b781caab83ecffe298d5dfc06aa
webferrol <xurxo@webferrol.com>
2020-03-04 19:48:58 +0100

    solución

--- js/script.js
+++ js/script.js
@@ -1,1 +1,11 @@
 //Scritp 1 utiliza toggle para que al pulsar el primer enlace con nombre de clase "boton" añada al segundo formulario el nombre de clase "enlinea"
+
+document.addEventListener("DOMContentLoaded",e=>{
+
+    document.querySelectorAll(".boton")[0].addEventListener("click",e=>{
+        e.preventDefault();
+
+        document.querySelector(".f2").classList.toggle("enlinea");
+    })
+
+});
